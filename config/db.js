407b2742d704449c2def052d7e7c6d2def249de3
Sakshi Gawade <sakshigawade2808@gmail.com>
2026-08-02 22:13:54 +0530

    Connected project to the aiven database

--- config/db.js
+++ config/db.js
@@ -2,40 +2,23 @@ const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-connection.connect((err)=>{
-    if(err){
+connection.connect((err) => {
+    if (err) {
         console.log("Database Connection Failed");
-    }else{
-        console.log("Database Connected");
+        console.log(err);
+    } else {
+        console.log("Database Connected Successfully");
     }
 });
 
-connection.query("SELECT DATABASE() AS db", (err, result) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Connected Database:", result[0].db);
-    }
-});
-
-connection.query("SELECT DATABASE() AS db", (err, result) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Connected Database:", result[0].db);
-    }
-});
-
-connection.query("SHOW COLUMNS FROM chains", (err, result) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.table(result);
-    }
-});
 module.exports = connection;
